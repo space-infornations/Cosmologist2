91bdd141b164e26655788687ebb1dbc07c409668
space-infornations <kartikmonga217@gmail.com>
2025-02-01 14:42:24 +0530

    new

--- script/partical.js
+++ script/partical.js
@@ -1,3 +1,4 @@
  window.addEventListener('load', () => {
         document.querySelector('.loader').style.display = 'none';
+        document.body.style.overflow = 'auto'; // Re-enable scrollbar
     });
